7edd377f062e1de7fd3979dcfe46c314d66f33cb
0 <0>
2022-07-22 11:12:36 -0500

    0.1-movement: refactored input & x force functionality

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,61 +4,143 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody playerRigidBody;
+
     public bool gravityStatus;
-    public float ZForce;
+    public float zForce;
     public float xForce;
-    public bool inputDirection;
+    public bool isInput;
+    public string inputDirection;
 
     void Start()    // called before the first frame update
     {
-        setGravity(gravityStatus);
+        applyGravity();
         Debug.Log("Gravity Status: " + gravityStatus);
     }
-    
+
     void Update()   // called once per frame
     {
-        getInputDirection();
+        updateInput();
     }
     
     void FixedUpdate()  // named "Fixed"Update due to physics manipulation (Time.deltaTime?)
     {
-        // applies force to move playerRigidBody forward
-        addZForce(ZForce);
+        // applies force along z-axis to playerRigidBody
+        applyZForce();
 
-        // applies force to playerRigidBody based on keyboard input
-        setInputDirection();
+        // applies force along x-axis to playerRigidBody
+        movePlayer();
     }
 
-    bool getInputDirection()
+    /* movement:
+     - check for updated input in Update()
+     - store input
+     - add force to stored input in FixedUpdate() */
+    void movePlayer()
     {
-        return (inputDirection) ? Input.GetKeyDown(KeyCode.D) : Input.GetKeyDown(KeyCode.A);
-    }
-
-    void setInputDirection()    // sets inputDirection based on keyboard input
-    {
-        if (getInputDirection())
+        if (getIsInput() && getInputDirection() == "left")
         {
-            addXForce(xForce);
+            setXForce(-xForce);
+            applyXForce();
+        }
+        if (getIsInput() && getInputDirection() == "right")
+        {
+            setXForce(xForce);
+            applyXForce();
+        }
+    }
+
+    void updateInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            setIsInput(true);
+            setInputDirection("left");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            setIsInput(true);
+            setInputDirection("right");
         }
         else
         {
-            addXForce(-xForce);
+            setIsInput(false);
+            setInputDirection(null);
         }
     }
 
-    void addZForce(float force)
+    string getInputDirection()
     {
-        playerRigidBody.AddForce(0, 0, deltaTimeMultiplier(force));
+        return inputDirection;
     }
 
-    void addXForce(float force)
+    void setInputDirection(string input)
     {
-        playerRigidBody.AddForce(deltaTimeMultiplier(force), 0, 0);
+        try
+        {
+            inputDirection = input;
+        }
+        catch
+        {
+            Debug.Log("error setting input direction");
+        }
     }
 
-    void setGravity(bool status)
+    bool getIsInput()
     {
-        playerRigidBody.useGravity = status;
+        return isInput;
+    }
+
+    void setIsInput(bool status)
+    {
+        isInput = status;
+    }
+
+    // xForce
+    void applyXForce()
+    {
+        playerRigidBody.AddForce(deltaTimeMultiplier(getXForce()), 0, 0);
+    }
+
+    float getXForce()
+    {
+        return xForce;
+    }
+
+    void setXForce(float force)
+    {
+        xForce = force;
+    }
+
+    // zForce
+    void applyZForce()
+    {
+        playerRigidBody.AddForce(0, 0, deltaTimeMultiplier(getZForce()));
+    }
+
+    float getZForce()
+    {
+        return zForce;
+    }
+
+    void setZForce(float force)
+    {
+        zForce = force;
+    }
+
+    // gravity
+    void applyGravity()
+    {
+        playerRigidBody.useGravity = getGravityStatus();
+    }
+
+    bool getGravityStatus()
+    {
+        return gravityStatus;
+    }
+
+    void setGravityStatus(bool status)
+    {
+        gravityStatus = status;
     }
 
     float deltaTimeMultiplier(float input)  // multiplies input by Time.deltaTime to adjust framerate across cpu performance variation
