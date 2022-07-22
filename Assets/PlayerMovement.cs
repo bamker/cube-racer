9fa1588c9427d8f6725795825d88bef590f7d9cc
0 <0>
2022-07-21 23:56:56 -0500

    0.1-movement: added lateral movement; added a + d keyboard inputs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,31 +3,68 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public Rigidbody rb;
+    public Rigidbody playerRigidBody;
+    public bool gravityStatus;
+    public float ZForce;
+    public float xForce;
+    public bool inputDirection;
 
-    // Start is called before the first frame update
-    void Start()
+    void Start()    // called before the first frame update
     {
+        setGravity(gravityStatus);
+        Debug.Log("Gravity Status: " + gravityStatus);
+    }
+    
+    void Update()   // called once per frame
+    {
+        getInputDirection();
+    }
+    
+    void FixedUpdate()  // named "Fixed"Update due to physics manipulation (Time.deltaTime?)
+    {
+        // applies force to move playerRigidBody forward
+        addZForce(ZForce);
 
+        // applies force to playerRigidBody based on keyboard input
+        setInputDirection();
     }
 
-    // Update is called once per frame
-    void Update()
+    bool getInputDirection()
     {
-        
+        return (inputDirection) ? Input.GetKeyDown(KeyCode.D) : Input.GetKeyDown(KeyCode.A);
     }
 
-    // FixedUpdate for physics
-    private void FixedUpdate()
+    void setInputDirection()    // sets inputDirection based on keyboard input
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        if (getInputDirection())
+        {
+            addXForce(xForce);
+        }
+        else
+        {
+            addXForce(-xForce);
+        }
     }
 
-    void setGravityStatus(bool status)
+    void addZForce(float force)
     {
-        rb.useGravity = status;
-        string gravityStatus = ("Gravity Status: " + status);
-        Debug.Log(gravityStatus);
+        playerRigidBody.AddForce(0, 0, deltaTimeMultiplier(force));
+    }
+
+    void addXForce(float force)
+    {
+        playerRigidBody.AddForce(deltaTimeMultiplier(force), 0, 0);
+    }
+
+    void setGravity(bool status)
+    {
+        playerRigidBody.useGravity = status;
+    }
+
+    float deltaTimeMultiplier(float input)  // multiplies input by Time.deltaTime to adjust framerate across cpu performance variation
+    {
+        float deltaInput = input * Time.deltaTime;
+        return deltaInput;
     }
 
 }
